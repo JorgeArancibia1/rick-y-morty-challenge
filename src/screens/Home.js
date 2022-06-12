713ad4b8c1edf53b685d6fa881277519655515a8
Jorge <jarancibia@waypoint.com>
2022-06-12 00:25:22 -0400

    i18n added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Card } from "../components/Card";
+import { Navbar } from "../components/Navbar"
+import { useFetch } from "../hooks/useFetch.tsx";
+import { useTranslation } from "react-i18next";
 
 export const Home = () => {
+  // const { t } = useTranslation();
+
+  // const { loading, results } = useFetch();
+
+  // console.log('Home Results =>', results);
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>hola</h1>
+      <Navbar />
+      <section className="">
+        {/* {
+          results.map(card => {
+            return <Card {...card} key={card.id} />
+          })
+        } */}
+      </section>
+      <footer className="">
+        {/* <p>{t('footer.by')}<span>Jorge Arancibia</span></p> */}
+      </footer>
+    </div>
   )
 }
