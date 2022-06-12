631de478d82ca054502ef52172ee4d528cf168b4
Jorge <jarancibia@waypoint.com>
2022-06-12 17:14:40 -0400

    screens solid base built

--- src/screens/DetailCharacter.js
+++ src/screens/DetailCharacter.js
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getInfoOneCharacter } from '../services/getInfo'
-import { useEffect } from 'react';
+import { useFetchDetails } from '../hooks/useFetchDetails';
+import { useParams } from 'react-router-dom';
 
 export const DetailCharacter = () => {
-  const [character, setCharacter] = useState({})
-  const [stateInfo, setStateInfo] = useState({
-    dataCharacter: {},
-    loading: true,
-  });
-  const params = useParams()
+  const { id } = useParams()
 
-  useEffect(() => {
-    const fetchCharacter = async () => {
-      const data = await getInfoOneCharacter(params.id);
-      // setStateInfo({
-      //   dataCharacter: character,
-      //   loading: false,
-      // });
-    };
-    // const data = fetchCharacter()
-    setCharacter(fetchCharacter())
-  }, [])
+  const { info } = useFetchDetails('character', id);
 
-
-
-  console.log('CHARACTER => ', character);
+  console.log(info);
 
   return (
-    <div>DetailCharacter</div>
+    <div>
+      <h1>{info?.name}</h1>
+    </div>
   )
 }
+
+
+
+
+
+// created: "2017-11-04T18:50:21.651Z"
+// episode: (51) ['https://rickandmortyapi.com/api/episode/1', 'https://rickandmortyapi.com/api/episode/2', 'https://rickandmortyapi.com/api/episode/3', 'https://rickandmortyapi.com/api/episode/4', 'https://rickandmortyapi.com/api/episode/5', 'https://rickandmortyapi.com/api/episode/6', 'https://rickandmortyapi.com/api/episode/7', 'https://rickandmortyapi.com/api/episode/8', 'https://rickandmortyapi.com/api/episode/9', 'https://rickandmortyapi.com/api/episode/10', 'https://rickandmortyapi.com/api/episode/11', 'https://rickandmortyapi.com/api/episode/12', 'https://rickandmortyapi.com/api/episode/13', 'https://rickandmortyapi.com/api/episode/14', 'https://rickandmortyapi.com/api/episode/15', 'https://rickandmortyapi.com/api/episode/16', 'https://rickandmortyapi.com/api/episode/17', 'https://rickandmortyapi.com/api/episode/18', 'https://rickandmortyapi.com/api/episode/19', 'https://rickandmortyapi.com/api/episode/20', 'https://rickandmortyapi.com/api/episode/21', 'https://rickandmortyapi.com/api/episode/22', 'https://rickandmortyapi.com/api/episode/23', 'https://rickandmortyapi.com/api/episode/24', 'https://rickandmortyapi.com/api/episode/25', 'https://rickandmortyapi.com/api/episode/26', 'https://rickandmortyapi.com/api/episode/27', 'https://rickandmortyapi.com/api/episode/28', 'https://rickandmortyapi.com/api/episode/29', 'https://rickandmortyapi.com/api/episode/30', 'https://rickandmortyapi.com/api/episode/31', 'https://rickandmortyapi.com/api/episode/32', 'https://rickandmortyapi.com/api/episode/33', 'https://rickandmortyapi.com/api/episode/34', 'https://rickandmortyapi.com/api/episode/35', 'https://rickandmortyapi.com/api/episode/36', 'https://rickandmortyapi.com/api/episode/37', 'https://rickandmortyapi.com/api/episode/38', 'https://rickandmortyapi.com/api/episode/39', 'https://rickandmortyapi.com/api/episode/40', 'https://rickandmortyapi.com/api/episode/41', 'https://rickandmortyapi.com/api/episode/42', 'https://rickandmortyapi.com/api/episode/43', 'https://rickandmortyapi.com/api/episode/44', 'https://rickandmortyapi.com/api/episode/45', 'https://rickandmortyapi.com/api/episode/46', 'https://rickandmortyapi.com/api/episode/47', 'https://rickandmortyapi.com/api/episode/48', 'https://rickandmortyapi.com/api/episode/49', 'https://rickandmortyapi.com/api/episode/50', 'https://rickandmortyapi.com/api/episode/51']
+// gender: "Male"
+// id: 2
+// image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+// location: {name: 'Citadel of Ricks', url: 'https://rickandmortyapi.com/api/location/3'}
+// name: "Morty Smith"
+// origin: {name: 'unknown', url: ''}
+// species: "Human"
+// status: "Alive"
+// type: ""
+// url: "https://rickandmortyapi.com/api/character/2"
--- src/services/getInfo.js
+++ src/services/getInfo.js
@@ -15,7 +15,6 @@ export const getInfoOneCharacter = async (id) => {
     const url = `https://rickandmortyapi.com/api/character/${id}`;
     const response = await fetch(url);
     const res = await response.json();
-    console.log(res);
 
     return res;
   } catch (error) {
@@ -35,12 +34,36 @@ export const getInfoLocation = async () => {
   }
 };
 
+export const getInfoOneLocation = async (id) => {
+  try {
+    const url = `https://rickandmortyapi.com/api/location/${id}`;
+    const response = await fetch(url);
+    const res = await response.json();
+    console.log(res);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const getInfoEpisode = async () => {
   try {
     const url = 'https://rickandmortyapi.com/api/episode';
     const response = await fetch(url);
     const res = await response.json();
-    console.log(res);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getInfoOneEpisode = async (id) => {
+  try {
+    const url = `https://rickandmortyapi.com/api/episode/${id}`;
+    const response = await fetch(url);
+    const res = await response.json();
 
     return res;
   } catch (error) {
